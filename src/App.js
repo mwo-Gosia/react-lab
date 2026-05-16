5c859103d08c6b88281d705711f572f4db66635a
mszpunar <mszpunar@d17-426a-pc-03.ipa.ii.agh.edu.pl>
2026-05-16 11:27:40 +0200

    dodanie validationMessage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let validationMessage;
+    if (email.length < 10) {
+        validationMessage = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 15) {
+        validationMessage = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        validationMessage = <div>Twój adres e-mail jest stanowczo za długi.</div>;
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+            <p>Twój e-mail wielkimi literami to {email.toUpperCase()}</p>
+            <input type="text" value={email} onChange={handleChange}/>
+            {email.length >0 && <div>{validationMessage}</div>}
         </div>
     );
 }
