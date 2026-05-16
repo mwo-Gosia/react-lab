df00a603a12f507812ef61593678abd3f776709e
mszpunar <mszpunar@d17-426a-pc-03.ipa.ii.agh.edu.pl>
2026-05-16 11:46:01 +0200

    dodanie buttona Wyswietl email... i alertu

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     }
 
     let validationMessage;
-    if (email.length < 10) {
+    if (email.length < 5) {
         validationMessage = <div>Ale masz krótki adres!</div>;
     } else if (email.length < 15) {
         validationMessage = <div>Twój adres e-mail jest w sam raz.</div>;
@@ -23,8 +23,11 @@ function App() {
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}</h2>
             <p>Twój e-mail wielkimi literami to {email.toUpperCase()}</p>
-            <input type="text" value={email} onChange={handleChange}/>
             {email.length >0 && <div>{validationMessage}</div>}
+            <input type="text" value={email} onChange={handleChange}/>
+            <button type="button" onClick={() => alert(email)}>
+                Wyświetl mój e-mail w alercie
+            </button>
         </div>
     );
 }
